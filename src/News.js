603f4c678a745f080fe86ea8334b716819ef4263
leto1004 <leto1004@gmail.com>
2023-11-15 20:49:16 +0900

    test
    test

--- src/News.js
+++ src/News.js
@@ -1,8 +1,13 @@
 import NewList from "./components/NewsList";
+import Categories from "./components/Catagories";
+import { useState, useCallback } from "react";
 
 const News = () => {
+  const [category, setCategory]=useState('all');
+  const onSelect = (category) => setCategory(category);
   return (
     <>
+      <Categories category={category} onSelect={onSelect}></Categories>
       <NewList></NewList>
     </>
   );
